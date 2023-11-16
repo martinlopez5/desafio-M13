ffc476da6e58b89aeb9e7dec664477ac2c23dac8
Martin Lopez <martingustavolopez@gmail.com>
2023-11-16 17:19:27 -0300

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
+  access_key = "$AWS_ACCESS_KEY_ID"
+  secret_key = "$AWS_SECRET_ACCESS_KEY"
 }
