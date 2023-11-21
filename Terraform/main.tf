15669c2ccde73b77ec072784881c334ab5732753
Martin Lopez <martingustavolopez@gmail.com>
2023-11-21 16:58:03 -0300

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -70,6 +70,20 @@ resource "aws_security_group" "backend_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    protocol = "tcp"
+    from_port = 80
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol = "-1"
+    from_port = 0
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "sg-modulo13"
   }
